458fb27429611d40efa932e200dafb9614f36b50
JunJie.Z <cerci.jj@gmail.com>
2021-03-06 13:37:20 +0800

    增加错误异常信息打印

--- security-jjwt/src/main/java/com/googlecode/easyec/security/handler/AbstractAuthenticationFailureHandler.java
+++ security-jjwt/src/main/java/com/googlecode/easyec/security/handler/AbstractAuthenticationFailureHandler.java
@@ -1,6 +1,8 @@
 package com.googlecode.easyec.security.handler;
 
 import com.googlecode.easyec.security.support.AbstractHttpMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -18,6 +20,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 public abstract class AbstractAuthenticationFailureHandler extends AbstractHttpMessageHandler
     implements AuthenticationFailureHandler, AuthenticationEntryPoint, InitializingBean {
 
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException, ServletException {
         writeMessage(getFailureMessage(ex), APPLICATION_JSON_UTF8, response);
@@ -29,6 +33,7 @@ public abstract class AbstractAuthenticationFailureHandler extends AbstractHttpM
     }
 
     protected Object getFailureMessage(AuthenticationException e) {
+        logger.error(e.getMessage(), e);
         Map<String, Object> result = new HashMap<>();
         result.put("result_code", handleError(e));
         return result;
